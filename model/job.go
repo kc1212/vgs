6fb5c87230450471800bdc8fccc35edf55d89886
kc <kc04bc@gmx.com>
2016-03-01 18:11:57 +0100

    Added job status

--- model/job.go
+++ model/job.go
@@ -3,6 +3,18 @@ package model
 type Job struct {
 	id       int
 	duration int64
-	history  []string
-	status   int // may change
+	history  []string  // possibly improve the type?
+	status   JobStatus // is this necessary?
+}
+
+type JobStatus int
+
+const (
+	Waiting JobStatus = iota
+	Running
+	Finished
+)
+
+func (j *Job) appendHistory(x string) {
+	j.history = append(j.history, x)
 }
--- model/node.go
+++ model/node.go
@@ -1,33 +1,38 @@
 package model
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // the compute nodes that does the actual processing
 type Node struct {
 	running   bool
 	startTime int64
-	job       Job
+	job       Job // should this be nil if job does not exist?
 }
 
 func InitNodes(n int) []Node {
 	return make([]Node, n)
 }
 
+// note we're just copying the job
 func (n *Node) startJob(job Job) {
 	// this condition should not happen
-	if n.running {
+	if n.running || job.status != Waiting {
 		// TODO panic or return error?
-		panic(fmt.Sprintf("Node is already running!\n"))
+		panic(fmt.Sprintf("Cannot start job %v on node %v!\n", job, *n))
 	}
 	n.startTime = time.Now().Unix()
 	n.job = job
+	n.job.status = Running
 	n.running = true
 }
 
 func (n *Node) poll() {
 	now := time.Now().Unix()
 	if n.running && (now-n.startTime) > n.job.duration {
+		n.job.status = Finished
 		n.running = false
 	}
 }
