f5820d3da69f5ccc453e0a838143f9a11baf6c8d
kc1212 <kc04bc@gmx.com>
2016-03-12 15:24:18 +0100

    Implemented communication between GSs

--- gs.go
+++ gs.go
@@ -1,7 +1,28 @@
 package main
 
+import "log"
+import "os"
+import "time"
+import "strconv"
 import "github.com/kc1212/vgs/model"
 
 func main() {
-	model.StartGS()
+	id, e := strconv.Atoi(os.Args[1])
+	if e != nil {
+		log.Fatalf("id argument incorrect")
+	}
+
+	n, e := strconv.Atoi(os.Args[2])
+	if e != nil {
+		log.Fatalf("n argument incorrect")
+	}
+
+	gs := model.InitGS(id, n, 3000, "localhost:")
+	gs.Run()
+
+	time.Sleep(time.Second * 5)
+
+	gs.Elect()
+
+	time.Sleep(time.Second * 5)
 }
--- model/gs.go
+++ model/gs.go
@@ -2,27 +2,40 @@ package model
 
 import (
 	"log"
-	"math/rand"
+	"net"
+	"net/http"
 	"net/rpc"
+	"strconv"
 )
 
 // the grid scheduler
 type GS struct {
 	id       int
+	addr     string
 	others   []string // other GS's
 	clusters []string
 	leader   string // the lead GS
 	jobs     []Job
 }
 
-func StartGS() {
-	id := rand.Int()
-	others := *new([]string)   // TODO read from config file or have bootstrapper
-	clusters := *new([]string) // TODO see above
+type GSArgs struct {
+	SenderId int
+	MsgType  int // TODO
+}
+
+func InitGS(id int, n int, basePort int, prefix string) GS {
+	addr := prefix + strconv.Itoa(basePort+id)
+	// TODO read from config file or have bootstrapper
+	var otherGS []string
+	for i := 0; i < n; i++ {
+		if i != id {
+			otherGS = append(otherGS, prefix+strconv.Itoa(basePort+i))
+		}
+	}
+	var clusters []string // TODO see above
 	leader := ""
 	jobs := InitJobs(0) // start with zero jobs
-	gs := GS{id, others, clusters, leader, jobs}
-	gs.run()
+	return GS{id, addr, otherGS, clusters, leader, jobs}
 }
 
 // TODO how should the user submit request
@@ -30,24 +43,61 @@ func StartGS() {
 
 // TODO only one cluster at the moment, need to generalise
 // runs the main loop
-func (gs *GS) run() {
+func (gs *GS) Run() {
+	// initialise GS as RPC server
+	gs.startRPC()
+
 	// initialise RPC for calling cluster prodecures
-	remote, err := rpc.DialHTTP("tcp", "localhost:1234")
-	if err != nil {
-		log.Fatal("dialing:", err)
-	}
+	// remote, err := rpc.DialHTTP("tcp", "localhost:1234")
+	// if err != nil {
+	// 	log.Fatal("dialing:", err)
+	// }
 
-	args := ResManArgs{42}
-	var reply int
-	err = remote.Call("ResMan.AddJob", args, &reply)
-	if err != nil {
-		log.Fatal("rm error:", err)
-	}
+	// args := ResManArgs{42}
+	// var reply int
+	// err = remote.Call("ResMan.AddJob", args, &reply)
+	// if err != nil {
+	// 	log.Fatal("rm error:", err)
+	// }
 
-	// initialise RPC for receiving requests
-	for {
-		// TODO get all the clusters
-		// TODO arrange them in loaded order
-		// TODO allocate *all* jobs
+	// // initialise RPC for receiving requests
+	// for {
+	// 	// TODO get all the clusters
+	// 	// TODO arrange them in loaded order
+	// 	// TODO allocate *all* jobs
+	// }
+}
+
+// send messages to procs with higher id
+func (gs *GS) Elect() {
+	// TODO maybe keep tcp connection open?
+	for i := range gs.others {
+		remote, e := rpc.DialHTTP("tcp", gs.others[i])
+		if e != nil {
+			log.Fatal("Dialing failed: ", e)
+		}
+		args := GSArgs{gs.id, 0}
+		var reply int
+		if e := remote.Call("GS.RecvMsg", args, &reply); e != nil {
+			log.Fatal("GS.RecvMsg failed: ", e)
+		}
 	}
+}
+
+func (gs *GS) RecvMsg(args *GSArgs, reply *int) error {
+	log.Printf("Msg received %v\n", *args)
+	*reply = 1
+	return nil
+}
+
+func (gs *GS) startRPC() {
+	// initialise RPC
+	log.Printf("Initialising RPC on addr %v\n", gs.addr)
+	rpc.Register(gs)
+	rpc.HandleHTTP()
+	l, e := net.Listen("tcp", gs.addr)
+	if e != nil {
+		log.Fatal("listen error:", e)
+	}
+	go http.Serve(l, nil)
 }
