5ad5e152ffaa955c06faed9b104e01f63249ae37
kc1212 <kc04bc@gmx.com>
2016-04-04 00:17:27 +0200

    Worker pool WIP

--- model/worker.go
+++ model/worker.go
@@ -5,6 +5,8 @@ import (
 	"time"
 )
 
+import "github.com/kc1212/vgs/common"
+
 // Worker is a compute nodes that does the actual processing
 type Worker struct {
 	startTime int64
@@ -25,4 +27,77 @@ func (n *Worker) startJob(job Job) {
 func (n *Worker) isRunning() bool {
 	now := time.Now().Unix()
 	return n.job.Duration > (now - n.startTime)
+}
+
+type WorkDone struct {
+	jobID    int
+	workerID int
+}
+
+type WorkerTask struct {
+	task  common.Task
+	jobID int
+}
+
+// work represent a single worker
+func work(workerID int, taskChan <-chan WorkerTask, resultChan chan<- WorkDone) {
+	for {
+		select {
+		case mytask := <-taskChan:
+			mytask.task()
+			resultChan <- WorkDone{mytask.jobID, workerID}
+		}
+	}
+}
+
+func runWorkers(n int, tasksChan <-chan WorkerTask, capReq <-chan int, capResp chan<- int) {
+	// initialisation
+	workerChans := make([]chan WorkerTask, n)
+	for i := range workerChans {
+		workerChans[i] = make(chan WorkerTask)
+	}
+	doneChan := make(chan WorkDone)
+	busyFlags := make([]bool, n)
+
+	// start all workers
+	for i := 0; i < n; i++ {
+		go work(i, workerChans[i], doneChan)
+	}
+
+	// handle new jobs and assign them to workers
+	// cap{Req,Resp} is for reporting the current capacity
+	// block until there's something to do
+	for {
+		select {
+		case <-capReq:
+			capResp <- countFree(busyFlags)
+		case task := <-tasksChan:
+			i := nextWorker(busyFlags)
+			busyFlags[i] = true
+			workerChans[i] <- task
+		case done := <-doneChan:
+			busyFlags[done.workerID] = false
+			// TODO inform GS
+		}
+	}
+}
+
+func countFree(running []bool) int {
+	cnt := 0
+	for _, x := range running {
+		if !x {
+			cnt++
+		}
+	}
+	return cnt
+}
+
+func nextWorker(running []bool) int {
+	for i := range running {
+		if !running[i] {
+			return i
+		}
+	}
+	log.Panic("GS shouldn't send jobs to a busy RM.")
+	return -1 // unreachable
 }
