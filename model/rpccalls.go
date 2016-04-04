e662311c12f4eb7db27666a1bf86cc4fcd05dbfb
kc1212 <kc04bc@gmx.com>
2016-04-05 00:17:29 +0200

    Streamlined RPC functions

--- model/rpccalls.go
+++ model/rpccalls.go
@@ -2,75 +2,42 @@ package model
 
 import (
 	"log"
-	"net/rpc"
 )
 
 import "github.com/kc1212/virtual-grid/common"
 
-// TODO a lot of repeated code here, need to be generalised
-
 func rpcSendMsgToRM(addr string, args *RPCArgs) (int, error) {
 	log.Printf("Sending message %v to %v\n", *args, addr)
-	reply := -1
-	remote, e := rpc.DialHTTP("tcp", addr)
-	if e != nil {
-		log.Printf("Node %v not online (DialHTTP)\n", addr)
-		return reply, e
-	}
-	common.RemoteCallNoFail(remote, "ResMan.RecvMsg", args, &reply)
-	return reply, remote.Close()
+	reply, e := common.DialAndCallNoFail(addr, "ResMan.RecvMsg", args)
+	return reply, e
 }
 
 // rpcAddJobsToRM creates an RPC connection with a ResMan and does one remote call on AddJob.
 func rpcAddJobsToRM(addr string, args *[]Job) (int, error) {
 	log.Printf("Sending job to RM on %v\n", addr)
-	reply := -1
-	remote, e := rpc.DialHTTP("tcp", addr)
-	if e != nil {
-		log.Printf("Node %v not online (DialHTTP)\n", addr)
-		return reply, e
-	}
-	common.RemoteCallNoFail(remote, "ResMan.AddJob", args, &reply)
-	return reply, remote.Close()
+	reply, e := common.DialAndCallNoFail(addr, "ResMan.AddJob", args)
+	return reply, e
 }
 
 // sendMsgToGS creates an RPC connection with another GridSdr and does one remote call on RecvMsg.
 func rpcSendMsgToGS(addr string, args *RPCArgs) (int, error) {
 	log.Printf("Sending message %v to GS on %v\n", *args, addr)
-	reply := -1
-	remote, e := rpc.DialHTTP("tcp", addr)
-	if e != nil {
-		log.Printf("Node %v not online (DialHTTP)\n", addr)
-		return reply, e
-	}
-	common.RemoteCallNoFail(remote, "GridSdr.RecvMsg", args, &reply)
-	return reply, remote.Close()
+	reply, e := common.DialAndCallNoFail(addr, "GridSdr.RecvMsg", args)
+	return reply, e
 }
 
 // rpcAddJobsToGS is a remote call that calls `RecvJobs`.
 // NOTE: this function should only be executed when CS is obtained.
 func rpcSyncJobs(addr string, jobs *[]Job) (int, error) {
 	log.Printf("Syncing jobs %v with GS on %v\n", *jobs, addr)
-	reply := -1
-	remote, e := rpc.DialHTTP("tcp", addr)
-	if e != nil {
-		log.Printf("Node %v not online (DialHTTP)\n", addr)
-		return reply, e
-	}
-	common.RemoteCallNoFail(remote, "GridSdr.RecvJobs", jobs, &reply)
-	return reply, remote.Close()
+	reply, e := common.DialAndCallNoFail(addr, "GridSdr.RecvJobs", jobs)
+	return reply, e
 }
 
 func rpcSyncScheduledJobs(addr string, jobs *[]Job) (int, error) {
 	log.Printf("Syncing scheduled jobs %v with GS %v\n", *jobs, addr)
-	reply := -1
-	remote, e := rpc.DialHTTP("tcp", addr)
-	if e != nil {
-		log.Printf("Node %v not online (DialHTTP)\n", addr)
-		return reply, e
-	}
-	common.RemoteCallNoFail(remote, "GridSdr.RecvScheduledJobs", jobs, &reply)
-	return reply, remote.Close()
+	reply, e := common.DialAndCallNoFail(addr, "GridSdr.RecvScheduledJobs", jobs)
+	return reply, e
 }
 
 func rpcDropJobs(addr string, n int) (int, error) {
@@ -78,12 +45,12 @@ func rpcDropJobs(addr string, n int) (int, error) {
 	return reply, e
 }
 
-func rpcSyncCompletedJobs(addr string, js *[]int64) (int, error) {
-	reply, e := common.DialAndCallNoFail(addr, "GridSdr.SyncCompletedJobs", js)
+func rpcSyncCompletedJobs(addr string, jobs *[]int64) (int, error) {
+	reply, e := common.DialAndCallNoFail(addr, "GridSdr.SyncCompletedJobs", jobs)
 	return reply, e
 }
 
-func rpcRemoveCompletedJobs(addr string, js *[]int64) (int, error) {
-	reply, e := common.DialAndCallNoFail(addr, "GridSdr.RemoveCompletedJobs", js)
+func rpcRemoveCompletedJobs(addr string, jobs *[]int64) (int, error) {
+	reply, e := common.DialAndCallNoFail(addr, "GridSdr.RemoveCompletedJobs", jobs)
 	return reply, e
 }
