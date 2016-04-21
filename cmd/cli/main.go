8a9d010a015b93768019f7318aad52f8fa0b5033
kc1212 <kc04bc@gmx.com>
2016-04-21 19:29:20 +0200

    No need interactive UI

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/rpc"
-	"os"
 	"time"
 )
 
 import "github.com/kc1212/virtual-grid/model"
 
-// TODO add interactive option
 func main() {
-	cli()
-}
-
-func cli() {
 	addr := flag.String("addr", "localhost:3000", "address:port of the grid scheduler")
 	jobsCount := flag.Int("count", 1, "the number of jobs to add")
 	duration := flag.Int64("duration", 0, "the duration for the jobs (default is a random value)")
@@ -52,27 +45,19 @@ func cli() {
 	reply := -1
 	remote, e := rpc.DialHTTP("tcp", *addr)
 	if e != nil {
-		log.Printf("Node %v is not online, make sure to use the correct address?\n", *addr)
+		log.Printf("Node %v is not online, make sure to use the correct address? %v\n", *addr, e.Error())
 		return
 	}
 
 	if *nodeType == "gs" {
 		if e := remote.Call("GridSdr.AddJobsViaUser", &jobs, &reply); e != nil {
 			log.Printf("Remote call GridSdr.AddJobsViaUser failed on %v, %v\n", *addr, e.Error())
+			return
 		}
 	} else if *nodeType == "rm" {
 		if e := remote.Call("ResMan.AddJobsViaUser", &jobs, &reply); e != nil {
 			log.Printf("Remote call ResMan.AddJobsViaUser failed on %v, %v\n", *addr, e.Error())
+			return
 		}
-	}
-}
-
-func interactive() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text()) // Println will add back the final '\n'
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 }
