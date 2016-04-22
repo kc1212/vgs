c803138c6ee15cc0a5a454cc3ac0dba41d380f31
kc1212 <kc04bc@gmx.com>
2016-04-22 10:48:03 +0200

    Specify worker count in run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,10 @@ set -e
 
 function usage {
     echo "usage:"
-    echo "./run.sh <gs count> <rm count> [<my addr>] [<discosrv addr:port>]"
+    echo "./run.sh <gs count> <rm count> <worker count> [<my addr>] [<discosrv addr:port>]"
 }
 
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 3 ]; then
     echo "incorrect number of arguments"
     usage
     exit 1
@@ -16,22 +16,23 @@ fi
 gs_start=3001
 rm_start=3101
 
-# set the number of GS and RM
+# set the number of GS and RM and Worker nodes
 gs_end=$(printf "30%02d" "$1")
 rm_end=$(printf "31%02d" "$2")
+worker_count="$3"
 
 # set my address
 # alternatively we can set it automatically by using the following
 # dig +short myip.opendns.com @resolver1.opendns.com
 my_addr=localhost
-if [ -n "$3" ]; then
-    my_addr="$3"
+if [ -n "$4" ]; then
+    my_addr="$4"
 fi
 
 # set discovery server address
 discosrv_addr="localhost:3333"
-if [ -n "$4" ]; then
-    discosrv_addr="$4"
+if [ -n "$5" ]; then
+    discosrv_addr="$5"
 else
     # start discosrv if we're using the default address
     ./bin/discosrv 1>&2 2>"$HOME/tmp/discosrv.log" &
@@ -46,6 +47,6 @@ done
 
 # start the RMs
 for i in $(seq "$rm_start" "$rm_end"); do
-    ./bin/resman -addr "$my_addr:$i" -id "$RANDOM" -discosrv "$discosrv_addr" 1>&2 2>"$HOME/tmp/resman.$i.log" &
+    ./bin/resman -addr "$my_addr:$i" -id "$RANDOM" -discosrv "$discosrv_addr" -nodes "$worker_count" 1>&2 2>"$HOME/tmp/resman.$i.log" &
     usleep 200000
 done
